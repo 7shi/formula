83eec1093d72185cf560417c03b82e52f5ea6271
7shi <7shi@live.jp>
2013-02-12 14:47:29 +0900

    式を残して計算

--- Formula/Main.cs
+++ Formula/Main.cs
@@ -4,6 +4,16 @@ using System.Linq;
 
 namespace Formula
 {
+	class Value
+	{
+		public int n;
+	}
+
+	class Add
+	{
+		public Value x, y;
+	}
+
 	class MainClass
 	{
 		public static void Main (string[] args)
@@ -13,6 +23,11 @@ namespace Formula
 			int c = a + b;
 			Console.WriteLine (c);
 			Console.WriteLine ("{0} + {1} = {2}", a, b, c);
+
+			var a2 = new Value { n = 1 };
+			var b2 = new Value { n = 2 };
+			var c2 = new Add { x = a2, y = b2 };
+			Console.WriteLine ("{0} + {1} = {2}", a2, b2, c2);
 		}
 	}
 }
